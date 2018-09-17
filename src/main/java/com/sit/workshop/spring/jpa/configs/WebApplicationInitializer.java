f0e4612f20f2f495d479025d13a07b1697cbeccb
SuksawatTawana <suksawat.taw@northbkk.ac.th>
2018-09-17 17:27:43 +0700

    update config database

--- src/main/java/com/sit/workshop/spring/jpa/configs/WebApplicationInitializer.java
+++ src/main/java/com/sit/workshop/spring/jpa/configs/WebApplicationInitializer.java
@@ -1,5 +1,22 @@
 package com.sit.workshop.spring.jpa.configs;
 
-public class WebApplicationInitializer {
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] { DataAccessConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { RESTfulAppConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
 
 }
